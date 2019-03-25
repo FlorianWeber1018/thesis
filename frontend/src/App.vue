69c2d0bffe03f82ac46759a294620e6445221db1
florian weber <florian.weber@live.de>
2019-03-25 11:20:14 +0100

    added UML stuff

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <ohead></ohead>
+  </v-app>
+</template>
+
+<script>
+import ohead from './views/ohead.vue'
+export default {
+  name: 'App',
+  components: {
+    ohead
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/ohead.vue
@@ -0,0 +1,18 @@
+<template>
+  <onav></onav>
+</template>
+
+<script>
+import onav from '@/components/onav.vue'
+  export default {
+    name: 'ohead',
+    components:{
+      onav
+    },
+    data () {
+      return {
+        //
+      }
+    }
+  }
+</script>
